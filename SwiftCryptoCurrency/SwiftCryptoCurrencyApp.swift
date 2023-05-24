1e534683184b5c4a28debf32313dbe83ddc176f4
Javierb1219 <bonillaculture1219@gmail.com>
2023-05-24 16:46:08 -0700

    Initial Commit

---
+++ SwiftCryptoCurrency/SwiftCryptoCurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCryptoCurrencyApp.swift
+//  SwiftCryptoCurrency
+//
+//  Created by Javier Bonilla on 5/24/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCryptoCurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
